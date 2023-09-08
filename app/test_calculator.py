62d6de6b1a949fcf71a34a990372da379f7f66c3
Tom Hu <tomhu1096+gpg@gmail.com>
2023-09-08 10:13:13 -0700

    fix: create new test

--- app/test_calculator.py
+++ app/test_calculator.py
@@ -28,3 +28,6 @@ def test_divide():
     assert Calculator.divide(0, 2.0) == 0
     assert Calculator.divide(-4, 2.0) == -2.0
     # assert Calculator.divide(2.0, 0.0) == 'Cannot divide by 0'
+
+def new_test():
+    assert Calculator.add(1, 4) == 5.0
