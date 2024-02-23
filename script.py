2a85f2d189cf1cc2fc84a1d5a274c0c48c464002
Tom Hu <thomas.hu@sentry.io>
2024-02-23 15:45:53 -0800

    fix: test CLI version

--- script.py
+++ script.py
@@ -1,6 +1,17 @@
 import getpass
 import os
+import subprocess
+import traceback
 
 if __name__=="__main__":
     print('hello')
     print(getpass.getuser())
+
+    try:
+        subprocess.run(
+            ["git", "rev-parse", "HEAD^@"], capture_output=True
+        )
+    except Exception as e:
+        print(e.message)
+        print(e)
+        print(traceback.print_exc())
